0de58c7ce7678da92da761a4277fa943a9365749
enthusiastica <williamcambell682@gmail.com>
2023-07-07 12:53:32 -0600

    feat: sidenav static page finish

--- src/app/core/containers/sidenav/sidenav.component.ts
+++ src/app/core/containers/sidenav/sidenav.component.ts
@@ -10,15 +10,15 @@ export class SidenavComponent implements OnInit {
 	showMenu = true;
 	responsiveView = false;
 	items = [
-		{ 'iconURL':'../../../../assets/icons/gold-coin-icon.png', 'itemText': 'Hello'},
-		{ 'iconURL':'../../../../assets/icons/gold-coin-icon.png', 'itemText': 'About Us'},
-		{ 'iconURL':'../../../../assets/icons/gold-coin-icon.png', 'itemText': 'Question / Answer'},
-		{ 'iconURL':'../../../../assets/icons/gold-coin-icon.png', 'itemText': 'Play with ChessCoin'},
-		{ 'iconURL':'../../../../assets/icons/gold-coin-icon.png', 'itemText': 'ChessCoin Shop'},
-		{ 'iconURL':'../../../../assets/icons/gold-coin-icon.png', 'itemText': 'Play for free'},
-		{ 'iconURL':'../../../../assets/icons/gold-coin-icon.png', 'itemText': 'Play with a Friend'},
-		{ 'iconURL':'../../../../assets/icons/gold-coin-icon.png', 'itemText': 'Contact Us'},
-		{ 'iconURL':'../../../../assets/icons/gold-coin-icon.png', 'itemText': 'Wallet Withdraw'},
+		{ 'iconURL':'../../../../assets/icons/updated/home.svg', 'itemText': 'Home'},
+		{ 'iconURL':'../../../../assets/icons/updated/about us.svg', 'itemText': 'About Us'},
+		{ 'iconURL':'../../../../assets/icons/updated/question icon gold.svg', 'itemText': 'Question / Answer'},
+		{ 'iconURL':'../../../../assets/icons/updated/coins gold icon.svg', 'itemText': 'Play with ChessCoin'},
+		{ 'iconURL':'../../../../assets/icons/updated/icon hand with coin gols.svg', 'itemText': 'ChessCoin Shop'},
+		{ 'iconURL':'../../../../assets/icons/updated/coin play for free gold.svg', 'itemText': 'Play for free'},
+		{ 'iconURL':'../../../../assets/icons/updated/cubo gold png.svg', 'itemText': 'Play with a Friend'},
+		{ 'iconURL':'../../../../assets/icons/updated/contact us icon gold.svg', 'itemText': 'Contact Us'},
+		{ 'iconURL':'../../../../assets/icons/updated/icon hand with coin gols.svg', 'itemText': 'Wallet Withdraw'},
 	]
 
 	constructor() { }
